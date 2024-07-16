db01c11d992eb15ca43a8545f7581584f4fcb41f
Dan Deboer <daniel.s.deboer@gmail.com>
2024-07-16 09:22:19 -0400

    sentry sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			input: ['resources/app/main.ts'],
-			refresh: true,
-		}),
-		vue(),
-		vuetify(),
-	],
+    plugins: [laravel({
+        input: ['resources/app/main.ts'],
+        refresh: true,
+    }), vue(), vuetify(), sentryVitePlugin({
+        org: "de-boer-tool",
+        project: "dsd-js"
+    })],
+
+    build: {
+        sourcemap: true
+    }
 })
